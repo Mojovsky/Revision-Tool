209f036090a05d33f2746604fcb0b96c071f82a9
mojovsky <danielderewonko@gmail.com>
2024-03-14 16:00:20 +0000

    user interaction logic for adding questions

---
+++ user_interaction.py
@@ -0,0 +1,36 @@
+def get_question_type(
+    prompt: str = "Enter your choice by appropriate number\n1. Multiple choice question\n2. Free form question\n\n",
+):
+    while True:
+        user_choice = input(prompt).strip()
+        if user_choice in ("1", "2"):
+            if user_choice == "1":
+                return "multiple_choice"
+            return "free_form"
+        else:
+            raise ValueError("Please choose one of the two options (1 or 2)")
+
+
+def get_question(question_type: str):
+    question_text = input("Please enter the question: ").strip()
+    if not question_text:
+        raise ValueError("Question cannot be empty")
+    answer = input("Please enter the correct answer: ").strip()
+    if not answer:
+        raise ValueError("Answer cannot be empty")
+    if question_type == "multiple_choice":
+        choices = []
+        for i in range(3):
+            while True:
+                choice = input(f"Please enter invalid choice nr. {i+1}: ")
+                if choice:
+                    choices.append(choice)
+                    break
+                else:
+                    raise ValueError("Invalid choice cannot be empty")
+
+        return question_type, question_text, answer, choices
+    return question_type, question_text, answer
+
+
+print(get_question("multiple_choice"))
