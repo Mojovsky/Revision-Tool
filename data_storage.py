5edd884c42794284395aa7e1586c5b2ee0bcb871
mojovsky <danielderewonko@gmail.com>
2024-03-15 11:50:14 +0000

    refactor save_question for simplicity, add update_question function

--- data_storage.py
+++ data_storage.py
@@ -9,9 +9,21 @@ def load_questions(filename: str):
         raise FileNotFoundError(f"Error: file {filename} not found")
 
 
-def save_question(filename: str, question: dict):
-    questions = load_questions(filename)
+def save_question(filename: str, question: dict, questions):
     questions.append(question)
 
     with open(filename, "w") as f:
         json.dump(questions, f, indent=4)
+
+
+def update_question(filename: str, question_id: str, new_value: dict, questions):
+    question_found = False
+    for index, data in enumerate(questions):
+        if data["question_id"] == question_id:
+            questions[index].update(new_value)
+            question_found = True
+            break
+    if question_found:
+        with open(filename, "w") as f:
+            json.dump(questions, f, indent = 4)
+        
