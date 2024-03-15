013f3f5627b29f50e9cf61be19a62b01e43cafd4
mojovsky <danielderewonko@gmail.com>
2024-03-15 11:49:47 +0000

    add function for changing status of a question

--- data_manipulation.py
+++ data_manipulation.py
@@ -1,5 +1,5 @@
 from questions import Question
-from data_storage import load_questions, save_question
+from data_storage import load_questions, save_question, update_question
 from user_interaction import get_question_type, get_question, line_break
 
 
@@ -17,8 +17,7 @@ def create_index(filename):
 
 
 def add_question():
-    filename = "questions.json"
-    question_id = create_index(filename)
+    question_id = create_index("questions.json")
     question_type = get_question_type()
     if question_type == "multiple_choice":
         question_text, answer, choices = get_question(question_type)
@@ -34,10 +33,9 @@ def add_question():
             answer,
             choices if question_type == "multiple_choice" else None,
         )
-        questions = load_questions(filename)
+        questions = load_questions("questions.json")
         new_question = new_question.__dict__
-        questions = new_question
-        save_question(filename, questions)
+        save_question("questions.json", new_question, questions)
         print("Question added successfully!")
     except FileNotFoundError as e:
         print(f"Error loading or saving questions: {e}")
@@ -45,8 +43,8 @@ def add_question():
 
 def show_questions():
     l_break = line_break()
-    question_list = load_questions("questions.json")
-    for data in question_list:
+    questions = load_questions("questions.json")
+    for data in questions:
         question_id = data["question_id"]
         question_text = data["question_text"]
         question_active = data["question_active"]
@@ -57,4 +55,14 @@ def show_questions():
         print(f"{l_break}\nQuestion ID: {question_id}\nQuestion text: {question_text}\nQuestion stauts: {question_status}\nNumber of times question appeared: {number_of_occurrences}\nSuccess percentage: {answer_success_percentage}\n{l_break}")
 
 
-show_questions()
+def change_question_status(question_id: str):
+    questions = load_questions("questions.json")
+    for index, data in enumerate(questions):
+      if data["question_id"] == question_id:
+          questions[index]["question_active"] = not data["question_active"]
+          new_status = data["question_active"]
+    update_question("questions.json", question_id, {"question_active": new_status}, questions)
+    print("Question enabled/disabled successfully!")
+
+
+change_question_status("#1")
