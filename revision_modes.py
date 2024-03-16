a021aabf5501f0b764e3c3ff57dab6b772abb70f
mojovsky <danielderewonko@gmail.com>
2024-03-16 18:47:44 +0000

    introduce a module for revision modes

--- revision_modes.py
+++ revision_modes.py
@@ -1,1 +1,11 @@
 from questions import Question
+from data_storage import load_questions, update_question
+from data_manipulation import success_percentage_calc
+
+
+def practice_mode():
+    questions = load_questions()
+    if len(questions) < 5:
+        print("Need at least 5 questions in order to proceed. Please add more questions.")
+    while True:
+        ...
