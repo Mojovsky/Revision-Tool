f75a1792dbfcb82b613964887ce0206b8a94ec74
mojovsky <danielderewonko@gmail.com>
2024-03-17 16:24:22 +0000

    refactor data_storage and data_manipulation into classes

--- questions.py
+++ questions.py
@@ -1,21 +1,16 @@
-from data_storage import save_question
+import json
+from user_interaction import get_question_type, get_question, line_break
 
 
 class Question:
-
-    def __init__(
-        self,
-        question_id,
-        question_type,
-        question_text,
-        answer,
-        choices,
-    ):
+    def __init__(self, question_id, question_type, question_text, answer, choices=None):
         self.question_id = question_id
         self.question_type = question_type
         self.question_text = question_text
         self.answer = answer
-        self.choices = choices
+        self.choices = choices  # Optional for multiple-choice questions
+
+        # Initialize statistics within the class
         self.answer_success_percentage = "0%"
         self.question_active = True
         self.number_of_occurrences = 0
@@ -79,20 +74,98 @@ class Question:
             return False
 
 
+class QuestionStorage:
+
+    def __init__(self, filename: str):
+        self.filename = filename
+        self.questions = self.load_questions()
+
+    
+    def load_questions(self):
+        try:
+            with open(self.filename, "r") as f:
+                return json.load(f)
+        except FileNotFoundError:
+            raise FileNotFoundError(f"Error: file {self.filename} not found")
 
 
-#def test():
-    new_index = "#3"
-    new_question = {
-        "question_id": new_index,
-        "question_type": "multiple_choice",
-        "question_text": "What is the capital of Poland",
-        "answer": "Warsaw",
-        "choices": ["Berlin", "Prague", "Warsaw", "Madrid"],
-        "question_active": True,
-        "number_of_occurences": 0,
-        "correct_answers": 0,
-        "answer_success_percentage": "0%",
-    }
 
-    save_question("questions.json", new_question)
+    def add_question(self, new_question: Question):
+        self.questions.append(new_question.__dict__)
+
+        with open(self.filename, "w") as f:
+            json.dump(self.questions, f, indent=4)
+
+    def _create_index(self):
+        return f"#{len(self.questions) + 1}"
+    
+    def update_question(self, question_id: str, new_value: dict):
+        question_found = False
+        for index, data in enumerate(self.questions):
+            if data["question_id"] == question_id:
+                self.questions[index].update(new_value)
+                question_found = True
+                break
+        if question_found:
+            with open(self.filename, "w") as f:
+                json.dump(self.questions, f, indent = 4)
+
+
+
+class QuestionManipulation:
+
+    def __init__(self, filename: str):
+        self.storage = QuestionStorage(filename)
+    
+
+    def create_question(self):
+        try:
+            question_id = self.storage._create_index()
+            question_type = get_question_type()
+            if question_type == "multiple_choice":
+                question_text, answer, choices = get_question(question_type)
+                choices.append(answer)
+            else:
+                question_text, answer = get_question(question_type)
+                choices = None
+
+            new_question = Question(question_id, question_type, question_text, answer, choices)
+            self.storage.add_question(new_question)
+            print("Question added successfully!")
+        except FileNotFoundError as e:
+            print(f"Error loading or saving questions: {e}")
+
+
+    def change_question_status(self, question_id: str):
+        for index, data in enumerate(self.storage.questions):
+            if data["question_id"] == question_id:
+                self.storage.questions[index]["question_active"] = not data["question_active"]
+                new_status = data["question_active"]
+        self.storage.update_question(question_id, {"question_active": new_status})
+        print("Question enabled/disabled successfully!")
+
+
+    def show_questions(self):
+        l_break = line_break()
+        for question in self.storage.questions:
+            question_status = "Active" if question.question_active else "Disabled"
+
+            print(f"{l_break}\nQuestion ID: {question.question_id}\nQuestion text: {question.question_text}\nQuestion stauts: {question_status}\nNumber of times question appeared: {question.number_of_occurrences}\nSuccess percentage: {question.answer_success_percentage}\n{l_break}")
+
+    
+
+
+
+
+def main():
+    question_manipulation = QuestionManipulation("questions.json")
+    question_storage = QuestionStorage("questions.json")
+    questions = question_manipulation.storage.questions
+    #question_manipulation.create_question()
+    #question_storage.update_question("#2", {"question_active": True})
+    #question_manipulation.change_question_status("#2")
+    question_manipulation.show_questions()
+
+
+if __name__ == "__main__":
+    main()
