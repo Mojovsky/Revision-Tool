bfb61ef708a10d411dcf08ca52b739d78657e70d
mojovsky <danielderewonko@gmail.com>
2024-03-18 16:23:34 +0000

    improved practice mode to check condition for amount of questions and properly exiting.

--- revision_modes.py
+++ revision_modes.py
@@ -4,17 +4,19 @@ import random
 
 def practice_mode():
     storage = QuestionStorage("questions.json")
-    question_list = storage.load_questions()
-    if len(question_list) < 5:
-        return "Need at least 5 questions in order to proceed. Please add more questions."
-    while True:
+    manipulation = QuestionManipulation("questions.json")
+    exit_condition = False
+    if manipulation.check_active_questions() < 5:
+        print("Not enough questions to proceed. Please add more questions.")
+    while not exit_condition:
+        question_list = storage.load_questions()
         for q in question_list:
             question_obj = Question.map_values(q)
             if question_obj.question_active == False:
                 continue
             trigger = input("Press ENTER to continue or type DONE to end the program. ")
             if trigger == "DONE".lower():
-                break
+                exit_condition = True
             elif trigger == "":
                 user_input = input(question_obj._question_text)
                 if Question.check_answer(question_obj, user_input):
@@ -24,7 +26,9 @@ def practice_mode():
                 q.update(question_obj.__dict__)
             else:
                 pass
-        storage.save_questions(question_list)
+            storage.save_questions(question_list)
+
+        
 
 
 practice_mode()
