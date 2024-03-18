604a899e767fa8c88cff7090b81d80371c1fa65a
mojovsky <danielderewonko@gmail.com>
2024-03-18 22:18:40 +0000

    refactor for in terminal readability

--- main.py
+++ main.py
@@ -7,21 +7,21 @@ def main():
         print("Choose one of the following options by refering to its index: ")
         while True:
             manipulation = QuestionManipulation("questions.json")
-            user_choice = input("\n1. Add questions\n2. View questions/stats\n3. Disable/enable questions\n4. Practice mode\n5. Test mode")
+            user_choice = input("\n1. Add questions\n2. View questions/stats\n3. Disable/enable questions\n4. Practice mode\n5. Test mode\n")
             if user_choice == "1":
                 manipulation.create_new_question()
                 break
 
             elif user_choice == "2":
                 try:
-                    manipulation.show_all_questions
+                    manipulation.show_all_questions()
                     break
                 except ValueError as e:
                     print(f"Value Error: {e}")
 
             elif user_choice == "3":
                 try:
-                    user_input = ("Please input index number of the question: ")
+                    user_input = input("Please input index number of the question: ")
                     if "#" not in user_input:
                         user_input = f"#{user_input}"
                     manipulation.update_question_status(user_input)
@@ -50,3 +50,5 @@ def main():
                 print("Please choose one of the available options.")
                 continue
             
+
+main()
