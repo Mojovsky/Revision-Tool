566aac2c7e8a85d2c71d634cf213738b246c823a
mojovsky <danielderewonko@gmail.com>
2024-03-18 14:10:23 +0000

    create practice mode -> improve checking for amount condition, introduce weighted random choices

--- revision_modes.py
+++ revision_modes.py
@@ -1,11 +1,30 @@
-from questions import Question
-from data_storage import load_questions, update_question
-from data_manipulation import success_percentage_calc
+from questions import Question, QuestionStorage, QuestionManipulation
+import random
 
 
 def practice_mode():
-    questions = load_questions()
-    if len(questions) < 5:
-        print("Need at least 5 questions in order to proceed. Please add more questions.")
+    storage = QuestionStorage("questions.json")
+    question_list = storage.load_questions()
+    if len(question_list) < 5:
+        return "Need at least 5 questions in order to proceed. Please add more questions."
     while True:
-        ...
+        for q in question_list:
+            question_obj = Question.map_values(q)
+            if question_obj.question_active == False:
+                continue
+            trigger = input("Press ENTER to continue or type DONE to end the program. ")
+            if trigger == "DONE".lower():
+                break
+            elif trigger == "":
+                user_input = input(question_obj._question_text)
+                if Question.check_answer(question_obj, user_input):
+                    print("Correct!")
+                else:
+                    print("False")
+                q.update(question_obj.__dict__)
+            else:
+                pass
+        storage.save_questions(question_list)
+
+
+practice_mode()
