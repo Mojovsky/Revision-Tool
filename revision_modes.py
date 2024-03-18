1f0f7cc1194d96e400246a7145ede45ba98dff5d
mojovsky <danielderewonko@gmail.com>
2024-03-18 20:17:01 +0000

    make practice mode more concise

--- revision_modes.py
+++ revision_modes.py
@@ -5,28 +5,23 @@ import random
 def practice_mode():
     storage = QuestionStorage("questions.json")
     manipulation = QuestionManipulation("questions.json")
-    exit_condition = False
     if manipulation.check_active_questions() < 5:
         print("Not enough questions to proceed. Please add more questions.")
-    while not exit_condition:
+        return
+    while True:
         question_list = storage.load_questions()
         weights = [manipulation.calculate_weight(q.get("answer_success_percentage")) for q in question_list]
         selected_question = random.choices(question_list, weights=weights)[0]
         question_obj = Question.map_values(selected_question)
-        if question_obj.question_active == False:
+        if not question_obj.question_active:
             continue
         trigger = input("Press ENTER to continue or type DONE to end the program. ")
         if trigger == "DONE".lower():
-            exit_condition = True
+            break
         elif trigger == "":
             user_input = input(question_obj._question_text)
-            if Question.check_answer(question_obj, user_input):
-                print("Correct!")
-            else:
-                print("False")
+            print("Correct!" if Question.check_answer(question_obj, user_input) else "False")
             selected_question.update(question_obj.__dict__)
-        else:
-            pass
         storage.save_questions(question_list)
 
         
