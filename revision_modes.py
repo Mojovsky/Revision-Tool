d571119e38f6c347938583034a36aa125458e77e
mojovsky <danielderewonko@gmail.com>
2024-03-18 20:09:20 +0000

    fully working practice mode

--- revision_modes.py
+++ revision_modes.py
@@ -10,23 +10,24 @@ def practice_mode():
         print("Not enough questions to proceed. Please add more questions.")
     while not exit_condition:
         question_list = storage.load_questions()
-        for q in question_list:
-            question_obj = Question.map_values(q)
-            if question_obj.question_active == False:
-                continue
-            trigger = input("Press ENTER to continue or type DONE to end the program. ")
-            if trigger == "DONE".lower():
-                exit_condition = True
-            elif trigger == "":
-                user_input = input(question_obj._question_text)
-                if Question.check_answer(question_obj, user_input):
-                    print("Correct!")
-                else:
-                    print("False")
-                q.update(question_obj.__dict__)
+        weights = [manipulation.calculate_weight(q.get("answer_success_percentage")) for q in question_list]
+        selected_question = random.choices(question_list, weights=weights)[0]
+        question_obj = Question.map_values(selected_question)
+        if question_obj.question_active == False:
+            continue
+        trigger = input("Press ENTER to continue or type DONE to end the program. ")
+        if trigger == "DONE".lower():
+            exit_condition = True
+        elif trigger == "":
+            user_input = input(question_obj._question_text)
+            if Question.check_answer(question_obj, user_input):
+                print("Correct!")
             else:
-                pass
-            storage.save_questions(question_list)
+                print("False")
+            selected_question.update(question_obj.__dict__)
+        else:
+            pass
+        storage.save_questions(question_list)
 
         
 
