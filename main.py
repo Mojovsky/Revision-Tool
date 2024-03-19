544bcdfe98b2b47c9bba4675ec26938f5c30bc3b
mojovsky <danielderewonko@gmail.com>
2024-03-19 09:17:12 +0000

    refactor main for readability, expand error catching

--- main.py
+++ main.py
@@ -1,54 +1,62 @@
 from questions import QuestionManipulation
+import sys
 from revision_modes import practice_mode, test_mode
 
 
 def main():
     while True:
-        print("Choose one of the following options by refering to its index: ")
-        while True:
-            manipulation = QuestionManipulation("questions.json")
-            user_choice = input("\n1. Add questions\n2. View questions/stats\n3. Disable/enable questions\n4. Practice mode\n5. Test mode\n")
-            if user_choice == "1":
-                manipulation.create_new_question()
-                break
-
-            elif user_choice == "2":
-                try:
-                    manipulation.show_all_questions()
-                    break
-                except ValueError as e:
-                    print(f"Value Error: {e}")
-
-            elif user_choice == "3":
-                try:
-                    user_input = input("Please input index number of the question: ")
-                    if "#" not in user_input:
-                        user_input = f"#{user_input}"
-                    manipulation.update_question_status(user_input)
-                    break
-                except IndexError as e:
-                    print(f"Index Error: {e}")
-
-
-            elif user_choice == "4":
-                try:
-                    practice_mode()
-                    break
-                except ValueError as e:
-                    print(f"Value Error: {e}")
-
-
-            elif user_choice == "5":
-                try:
-                    test_mode()
-                    break
-                except ValueError as e:
-                    print(f"Value Error: {e}")
-
-
-            else:
-                print("Please choose one of the available options.")
-                continue
+        try:
+            display_menu()
+            user_selected_option = input("Enter your choice: ")
+            handle_user_choice(user_selected_option)
+        except(ValueError, IndexError, FileNotFoundError) as e:
+            print(f"Error: {e}")
+            input("Press ENTER to continue.")
+        except KeyboardInterrupt:
+            print("\nExiting program.")
+            break
             
 
-main()
+def display_menu():
+    print("\nChoose one of the following options:")
+    print("1. Add questions")
+    print("2. View questions/stats")
+    print("3. Disable/enable questions")
+    print("4. Practice mode")
+    print("5. Test mode")
+    print("Type 'DONE' to exit.")
+
+def handle_user_choice(user_choice):
+    manipulation = QuestionManipulation("questions.json")
+    if user_choice == "1":
+        manipulation.create_new_question()
+        print("Question added successfully!")
+
+    elif user_choice == "2":
+        manipulation.show_all_questions()
+        input("Here's a list of all the questions. Press ENTER to continue.")
+
+    elif user_choice == "3":
+        user_input = input("Please input index number of the question: ")
+        if "#" not in user_input:
+            user_input = f"#{user_input}"
+        manipulation.update_question_status(user_input)
+        input("Question enabled/disabled successfully! Press ENTER to continue.")
+    
+    elif user_choice == "4":
+        practice_mode()
+
+    elif user_choice == "5":
+        user_input = int(input("Select the number of questions for the test: \n"))
+        test_mode(user_input)
+        input("Press ENTER to continue.")
+
+    elif user_choice.lower() == "done":
+        print("Exiting program.")
+        sys.exit()
+    else:
+        print("Invalid choice. Please select one of the available options.")
+
+
+if __name__ == "__main__":
+    main()
